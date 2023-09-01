cb910dc34d60ae6fa6a80660fbd8505610c713d0
Mary Ann Jose <maryannjose129@gmail.com>
2023-09-01 08:49:10 +0530

    commit

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js';
-import { getDatabase , ref , push , onValue } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js';
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js';
 
 const firebaseConfig = {
-    databaseURL:"https://shoppinglist-1420e-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://shoppinglist-1420e-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const itemsDB = ref(database,"items")
+const itemsDB = ref(database, "items")
 
 const inputField = document.getElementById("text-field")
 const addButton = document.getElementById("add-button")
 const shoppinglist = document.getElementById("shopping-list")
 
-function clearInputField(){
-    inputField.value=""
+function clearInputField() {
+    inputField.value = ""
 }
 
-function clearShoppingList(){
-    shoppinglist.innerHTML=""
+function clearShoppingList() {
+    shoppinglist.innerHTML = ""
 }
 
-onValue(itemsDB, function(snapshot){
-    let items = Object.values(snapshot.val())
-    clearShoppingList()
-    for(let i=0;i<items.length;i++){
-        addItem(items[i])
+onValue(itemsDB, function (snapshot) {
+    if (snapshot.exists()) {
+        let items = Object.entries(snapshot.val())
+        clearShoppingList()
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i]
+            addItem(item)
+        }
+    }else{
+        clearShoppingList()
     }
 })
 
-function addItem(itemName){
-    shoppinglist.innerHTML += `<li>${itemName}</li>`
+function addItem(item) {
+    let itemId = item[0]
+    let itemName = item[1]
+    let itemElement = document.createElement("li")
+    itemElement.textContent = itemName
+    itemElement.addEventListener("dblclick", function () {
+        let removeItem = ref(database, `items/${itemId}`)
+        remove(removeItem)
+    })
+    shoppinglist.append(itemElement)
 }
 
-addButton.addEventListener("click",function (){
+addButton.addEventListener("click", function () {
     let itemName = inputField.value
-    push(itemsDB,itemName)
+    push(itemsDB, itemName)
     clearInputField()
     console.log("done")
 })
